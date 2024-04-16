2ca0039e5b5419f34604e2c6be260c78f79e36af
isak123isak <isak123isak@hotmail.com>
2024-04-16 13:46:51 +0200

    Javascript basics

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+function addTodo() {
+
+event.preventDefault();
+
+console.log("Is button clicked?")
+
+
+// const er en ærklæring
+//taskInputData er et variabelnavn
+//= er å erklære en verdi som skal lagres
+//document refererer til et HTML dokument
+//.queryselector() henter noe fra HTML
+//#TaskInput er id-navnet til elementet vi henter fra HTML
+//.value er noe vi henter til elementet
+//; betyr full stopp. Denne linjen med kode er ferdig
+const taskInputData = document.querySelector("#taskInput").value;
+console.log(taskInputData);
+// Definere et et element i dette tilfellet en liste og skriver den ut i terminalen.
+const newListItem = document.createElement("li");
+console.log(newListItem);
+//
+newListItem.textContent = taskInputData;
+//
+const taskList = document.querySelector("#taskList");
+console.log(taskList);
+//Legger til det siste  barne-elementet inne i (newListItem)
+taskList.appendChild(newListItem);
+
+}
+//
+const taskButton = document.querySelector("#taskButton");
+console.log(taskButton);
+//
+taskButton.addEventListener("click", addTodo);
+
 
